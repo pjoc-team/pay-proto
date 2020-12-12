a4422fd94d8f9c890433a32ca71253dd2e0c5e59
blademainer <blademainer@gmail.com>
2020-12-12 18:41:14 +0800

    support post raw body

--- scripts/generate_proto.sh
+++ scripts/generate_proto.sh
@@ -40,7 +40,7 @@ find $PROTO_HOME -name "*.proto" | while read proto; do
   proto_name="${file_name%.*}"
   echo "proto file: $proto dir: ${dir} file_name: $file_name proto_name: $proto_name out_dir: ${out_dir}"
   echo "generating proto..."
-  [[ "$language" == "go" ]] && addition=" --with-gateway --validate-out lang=go:/out "
+  [ "$language" == "go" ] && addition=" --with-gateway --validate-out lang=go:/out "
 #  docker run --rm -v "$dir":/defs -v "${out_dir}":/out blademainer/protoc-all:latest -i /defs -i /go/src -d /defs/ -l $language -o /out --lint $addition
   docker run --rm -v "$dir":/defs -v "${out_dir}":/out namely/protoc-all:1.33_1 -f $file_name -l $language -o /out --lint --with-validator $addition
 #  docker run --rm -v $dir:/defs -v ${IMPORT_HOME}:/input blademainer/protoc-all:latest -i /defs -i /input -i /go/src/ -d /defs/ -l go -o /defs --validate-out "lang=go:/defs" --with-gateway --lint $addition
